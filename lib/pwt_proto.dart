2510cd234eaec9179cc0894e4b77220420dfc4dc
Yohan Beschi <yohan.beschi@gmail.com>
2013-03-31 17:26:42 +0200

    The prototype is now a library (easier for presentation projects)

---
+++ lib/pwt_proto.dart
@@ -0,0 +1,12 @@
+library pwt_proto;
+
+export 'pwt/pwt.dart';
+export 'dart:async';
+export 'dart:html';
+export 'dart:math';
+export 'animation/pwt_animation.dart';
+export 'css/pwt_css.dart';
+export 'dnd/pwt_dnd.dart';
+export 'utils/pwt_utils.dart';
+export 'layouts/pwt_layouts.dart';
+export 'widgets/pwt_widgets.dart';
